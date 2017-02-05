7bd4e72429fa6ebce97a29ebbf045d369cb79044
zeb <zeb@cyclerouter.co.uk>
2017-02-05 18:28:43 +0000

    Add adepter GoF code

--- Adapter/GoF.h
+++ Adapter/GoF.h
@@ -2,4 +2,74 @@
 
 namespace AdapterPattern::GoF
 {
+	class Coord {
+	public:
+		Coord& operator+ (Coord&) { return *this; }
+	};
+
+	class Point {
+	public:
+		Point(Coord&, Coord&) {};
+	};
+	class Manipulator {};
+
+	class TextShape;
+	class TextManipulator : public Manipulator {
+	public:
+		TextManipulator(const TextShape*) {}
+	};
+
+	class Shape {
+	public:
+		Shape() {}
+		virtual void BoundingBox(
+			Point& bottomLeft, Point& topRight
+		) const {}
+		virtual Manipulator* CreateManipulator() const { return nullptr; }
+	};
+
+	class TextView {
+	public:
+		TextView() {}
+		void GetOrigin(Coord& x, Coord& y) const {}
+		void GetExtent(Coord& width, Coord& height) const {}
+		virtual bool IsEmpty() const {}
+	};
+
+	class TextShape : public Shape {
+	public:
+		TextShape(TextView*);
+
+		virtual void BoundingBox(
+			Point& bottomLeft, Point& topRight
+		) const;
+		virtual bool IsEmpty() const;
+		virtual Manipulator* CreateManipulator() const;
+	private:
+		TextView* _text;
+	};
+
+	TextShape::TextShape(TextView* t) {
+		_text = t;
+	}
+
+	void TextShape::BoundingBox(
+		Point& bottomLeft, Point& topRight
+	) const {
+		Coord bottom, left, width, height;
+
+		_text->GetOrigin(bottom, left);
+		_text->GetExtent(width, height);
+
+		bottomLeft = Point(bottom, left);
+		topRight = Point(bottom + height, left + width);
+	}
+
+	bool TextShape::IsEmpty() const {
+		return _text->IsEmpty();
+	}
+
+	Manipulator* TextShape::CreateManipulator() const {
+		return new TextManipulator(this);
+	}
 }
