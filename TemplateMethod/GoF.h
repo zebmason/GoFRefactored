0d320d811b0039164b7f2f96fb7f70c03ddf4fdc
zeb <zeb@cyclerouter.co.uk>
2017-02-06 17:11:42 +0000

    Add Template Method Pattern GoF code

--- TemplateMethod/GoF.h
+++ TemplateMethod/GoF.h
@@ -2,4 +2,31 @@
 
 namespace TemplateMethodPattern::GoF
 {
+	class View
+	{
+	protected:
+		void SetFocus() {}
+		void ResetFocus() {}
+		virtual void DoDisplay();
+	public:
+		void Display();
+	};
+
+	void View::Display() {
+		SetFocus();
+		DoDisplay();
+		ResetFocus();
+	}
+
+	void View::DoDisplay() { }
+
+	class MyView : public View
+	{
+	protected:
+		virtual void DoDisplay();
+	};
+
+	void MyView::DoDisplay() {
+		// render the view's contents 
+	}
 }
